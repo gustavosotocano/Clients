ee6b80853e89bf0f8dde86d763ff899cf9b32cef
gustavosotocano <gustavosotc@gmail.com>
2024-05-08 11:59:44 -0500

    Remove unused imports in Utils and ClientService
    The following unused imports have been removed from the Utils.java and ClientService.java files: BigInteger, ParseException, and SimpleDateFormat. This improves code readability and reduces clutter.

--- client-back/src/main/java/com/gml/service/ClientService.java
+++ client-back/src/main/java/com/gml/service/ClientService.java
@@ -9,8 +9,6 @@ import com.gml.util.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
--- client-back/src/main/java/com/gml/util/Utils.java
+++ client-back/src/main/java/com/gml/util/Utils.java
@@ -4,7 +4,6 @@ import com.gml.dto.ClienteDto;
 import com.gml.entity.Client;
 import lombok.extern.slf4j.Slf4j;
 
-import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
